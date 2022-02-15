08e4844f98dd61332d0643a2de9abb128dfc51f0
Jessy <jessy.etokabeka@gmail.com>
2022-02-15 15:00:25 +0100

    clean app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 // Import pages
-import { Home } from "./pages/pagesExport";
+// import { Home } from "./pages/pagesExport";
 
 function App() {
   return <div>Hello World !!</div>;
